b332e57648f37ac3950508cbd983cc125f06fbea
Alexandre Ghiti <aghiti@upmem.com>
2020-09-09 13:21:39 +0200

    fix

--- checksum_not_checksum/dpu/dpu.c
+++ checksum_not_checksum/dpu/dpu.c
@@ -47,11 +47,11 @@ int main()
     uint32_t tasklet_id = me();
 
     if (tasklet_id == 0) {
-        uint8_t wram_poison[256] = { 0 };
-        uint8_t mram_read_poison[256];
+        uint64_t wram_poison[32] = { 0 };
+        uint64_t mram_read_poison[32];
             mram_write(wram_poison, &DPU_BUFFER[0], 256);
             mram_read(&DPU_BUFFER[0], mram_read_poison, 256);
-            for (int i = 0; i < 256; ++i)
+            for (int i = 0; i < 32; ++i)
                 if (mram_read_poison[i])
                     halt();
     }
